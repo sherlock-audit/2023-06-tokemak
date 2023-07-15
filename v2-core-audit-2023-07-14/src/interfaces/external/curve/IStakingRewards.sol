12b60be0275cda3e2f27d0c24aeb641b67640fa0
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-07-15 11:17:58 +0200

    Delete v2-core-audit-2023-07-14 directory

--- v2-core-audit-2023-07-14/src/interfaces/external/curve/IStakingRewards.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-import { IERC20 } from "openzeppelin-contracts/token/ERC20/IERC20.sol";
-
-interface IStakingRewards {
-    function rewardsToken() external view returns (IERC20);
-}
