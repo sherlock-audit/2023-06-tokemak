0ff9441dde140632ce6cf513719c53c59f4f0558
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-07-15 11:01:16 +0200

    Delete v2-core-audit-2023-07-14 directory

--- v2-core-audit-2023-07-14/src/interfaces/external/convex/IConvexBoosterArbitrum.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-/// @notice main Convex contract(booster.sol) basic interface
-interface IConvexBoosterArbitrum {
-    /// @notice deposit into convex, receive a tokenized deposit
-    function deposit(uint256 _pid, uint256 _amount) external returns (bool);
-}
