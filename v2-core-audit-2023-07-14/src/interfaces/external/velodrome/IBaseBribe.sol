76eea6bb00f7a956c31e4e526785ea21c7d4c626
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-07-17 01:25:11 +0200

    Delete v2-core-audit-2023-07-14 directory

--- v2-core-audit-2023-07-14/src/interfaces/external/velodrome/IBaseBribe.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-interface IBaseBribe {
-    function rewards(uint256 index) external view returns (address);
-    function rewardsListLength() external view returns (uint256);
-}
--- v2-core-audit-2023-07-14/src/interfaces/external/velodrome/IInternalBribe.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-interface IInternalBribe {
-    function rewards(uint256 index) external view returns (address);
-    function rewardsListLength() external view returns (uint256);
-}
